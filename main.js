2a68c9df3580e7b415dcf17baaca31785f1796b8
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-04-04 13:44:25 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+for (let currentNumber = 0; currentNumber < 100; currentNumber++) {
+    if (currentNumber % 5 === 0) {
+    console.log("Chicken");
+} else if (currentNumber % 7 === 0) {
+    console.log("Monkey");
+} else {
+    console.log(currentNumber);
+    }
+}
